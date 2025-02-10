9c52f87032084dd3e811212af682f706431fc0d7
naim79992 <naim79992@gmail.com>
2025-02-10 20:37:37 +0600

    Add example of final method in Java with subclass demonstration

---
+++ Java_Basics/FinalMethod.java
@@ -0,0 +1,23 @@
+package Java_Basics;
+
+public class FinalMethod {
+    // This is a final method. It cannot be overridden by subclasses.
+    public final void displayMessage() {
+        System.out.println("This is a final method.");
+    }
+    // Subclass attempting to override the final method
+    public static class SubClass extends FinalMethod {
+        // Uncommenting the below method will cause a compilation error
+        // because displayMessage() is final in the superclass.
+        /*
+        @Override
+        public void displayMessage() {
+            System.out.println("Attempting to override the final method.");
+        }
+        */
+    }
+    public static void main(String[] args) {
+        FinalMethod obj = new FinalMethod();
+        obj.displayMessage(); // Calling the final method
+    }
+}
