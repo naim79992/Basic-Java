beb590b04411697133b01ad17ea8e3d2c41afdce
naim79992 <naim79992@gmail.com>
2025-02-10 20:12:17 +0600

    Add abstract class example with Vehicle and Car implementations

---
+++ Java_Basics/AbstractMethod.java
@@ -0,0 +1,23 @@
+package Java_Basics;
+
+abstract class Vehicle {
+    abstract void startEngine();
+    void stopEngine() {System.out.println("Engine stopped.");}
+}
+
+class Car extends Vehicle {
+    void startEngine() {System.out.println("Car engine started.");}
+}
+
+public class AbstractMethod {
+    public static void main(String[] args) {
+        // You cannot create an instance of an abstract class
+        // Vehicle myVehicle = new Vehicle(); // This will cause an error
+
+        // You need to create a subclass that provides implementations for the abstract methods
+        Vehicle myCar = new Car();
+        myCar.startEngine();
+        myCar.stopEngine();
+    }
+}
+
