6e7222d9364583b9414d6c62444bcf9fb25b2b18
naim79992 <mhnaim79992@gmail.com>
2025-06-13 11:46:57 +0600

    Refactor output messages in MyThread class for consistency

--- Java_Basics/Threads/byextend.java
+++ Java_Basics/Threads/byextend.java
@@ -2,12 +2,12 @@ package Java_Basics.Threads;
 
 class MyThread extends Thread {
     public void run() {
-        System.out.println("🧵 Thread চলছে: " + Thread.currentThread().getName());
+        System.out.println("Thread running: " + Thread.currentThread().getName());
     }
 }
 public class byextend{
     public static void main(String[] args) {
         MyThread t1 = new MyThread();
-        t1.start();  // Thread শুরু হয় run() method দিয়ে
-    }
+        t1.start();  // Thread starts in run() method
+}
 }
