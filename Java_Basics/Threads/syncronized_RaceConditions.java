e0e53efee747cb4887f63824b6b5bd9f7fb519f4
naim79992 <mhnaim79992@gmail.com>
2025-06-14 22:14:35 +0600

    ok

---
+++ Java_Basics/Threads/syncronized_RaceConditions.java
@@ -0,0 +1,30 @@
+package Java_Basics.Threads;
+
+class Counter {
+    int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+}
+
+public class syncronized_RaceConditions {
+    public static void main(String[] args) throws InterruptedException {
+        Counter c = new Counter();
+
+        Thread t1 = new Thread(() -> {
+            for(int i = 0; i < 1000; i++) c.increment();
+        });
+
+        Thread t2 = new Thread(() -> {
+            for(int i = 0; i < 1000; i++) c.increment();
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println("Count: " + c.count); // Always 2000
+    }
+}
