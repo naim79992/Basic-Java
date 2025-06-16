e62ec2f4ee33e03ea7795fcd84d96562d99f8aba
naim79992 <mhnaim79992@gmail.com>
2025-06-16 08:53:36 +0600

    Add nLineLambda class and launch configuration for lambda expression demonstration

---
+++ Java_Basics/Lambda_Expression/nLineLambda.java
@@ -0,0 +1,20 @@
+package Java_Basics.Lambda_Expression;
+
+@FunctionalInterface
+interface Vehicle2 {
+    void run(int speed, int gear);  // দুইটা parameter
+}
+
+public class nLineLambda {
+    public static void main(String[] args) {
+        Vehicle2 car = (speed, gear) -> {
+            System.out.println("Starting the car...");
+            System.out.println("Car speed: " + speed + " km/h");
+            System.out.println("Gear engaged: " + gear);
+            System.out.println("Driving smoothly!\n");
+        };
+
+        car.run(80, 3);
+        car.run(120, 5);
+    }
+}
